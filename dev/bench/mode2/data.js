03ff1e907ca0294d2631fd4befd1c19ecda6ab56
github-action-benchmark <github@users.noreply.github.com>
2026-09-24 08:16:54 +0000

    add ArcadeDB Mode 2 — Embedded (datagen-7_5-fb) (customSmallerIsBetter) benchmark result for 02e1d824a2ff1bfc1574bbdda7cb9b3c2344bfff

--- dev/bench/mode2/data.js
+++ dev/bench/mode2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790151958300,
+  "lastUpdate": 1790237814499,
   "repoUrl": "https://github.com/ArcadeData/ldbc_graphalytics_platforms_arcadedb",
   "entries": {
     "ArcadeDB Mode 2 — Embedded (datagen-7_5-fb)": [
@@ -1766,6 +1766,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "CDLP",
             "value": 9.49,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "02e1d824a2ff1bfc1574bbdda7cb9b3c2344bfff",
+          "message": "Bump actions/setup-java from 5.7.0 to 6.0.0 (#27)\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-09-08T08:35:44Z",
+          "url": "https://github.com/ArcadeData/ldbc_graphalytics_platforms_arcadedb/commit/02e1d824a2ff1bfc1574bbdda7cb9b3c2344bfff"
+        },
+        "date": 1790237814179,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "LOAD",
+            "value": 133.02,
+            "unit": "s"
+          },
+          {
+            "name": "PR",
+            "value": 0.58,
+            "unit": "s"
+          },
+          {
+            "name": "WCC",
+            "value": 0.66,
+            "unit": "s"
+          },
+          {
+            "name": "BFS",
+            "value": 0.21,
+            "unit": "s"
+          },
+          {
+            "name": "LCC",
+            "value": 12.27,
+            "unit": "s"
+          },
+          {
+            "name": "SSSP",
+            "value": 2.67,
+            "unit": "s"
+          },
+          {
+            "name": "CDLP",
+            "value": 9.32,
             "unit": "s"
           }
         ]
